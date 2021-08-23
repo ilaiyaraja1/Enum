8861d3a4fd9074e41460c0f08d77fd7c783ccea8
ilaiyaraja1 <ilaiyaraja.bca@gmail.com>
2021-08-23 22:43:42 +0530

    add weekdays
    here we print weekday using enums...

--- src/Days/everycommendays.java
+++ src/Days/everycommendays.java
@@ -1,30 +1,12 @@
 package Days;
 
 public class everycommendays {
-	
-	enum days{
-		monday,tuesday,wednesday,thursday,friday,saturday,sunday
-	}
-	
-	
-	public static void weekend(days d){
-		
-     	if(d.equals(days.sunday)){
-     				System.out.println(d+"is holyday");
-     		
-     			}
-     	else{
-     		System.out.println(d+"is working day");
-     	}
-}
-	
-	
-
+        enum day{monday,tuesday,wednesday,thursday,friday,saturday,sunday};
 	public static void main(String[] args) {
 		
-		for(days d:days.values()){
-			weekend(d);
-		}
+            for(day d:day.values()){
+            	System.out.println("days is :"+d);
+            }
 	}	
 }
 
